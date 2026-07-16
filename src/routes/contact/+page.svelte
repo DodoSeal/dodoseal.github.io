11862c6f9ed3c385cf72d28061c49fa60a69ea4a
DodoSeal <maxhmcd08@gmail.com>
2026-07-16 16:10:32 -0600

    Feat: Setup main pages

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Navbar from "$lib/assets/Navbar.svelte";
+	import { onMount } from "svelte";
+    let sitePath: string = $state("");
+
+    onMount(() => {
+        sitePath = window.location.pathname;
+    });
+</script>
+
+<div class="main-content center-content flex-col bg-backdrop">
+    <Navbar />
+    <p class="text-white">{sitePath}</p>
+</div>
