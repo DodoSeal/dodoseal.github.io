489a16efc9ebd4b6731fbc2c74b9d8002c7e5c64
DodoSeal <maxhmcd08@gmail.com>
2026-07-16 16:10:15 -0600

    Feat: View Transitions on every page (works w/ Svelte)

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { onNavigate } from '$app/navigation';
+
+	let { children } = $props();
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
+</script>
+
+<svelte:head>
+	<meta property="og:image" content={favicon}>
+	<link rel="icon" href={favicon} />
+	<title>Max McDaniel</title>
+</svelte:head>
+
+{@render children()}
