f1d0c2a18d6fcc0797c85390dabf7e664987c725
DodoSeal <maxhmcd08@gmail.com>
2026-07-16 16:09:54 -0600

    Feat: Error page handling

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { page } from "$app/state";
+	import Navbar from "$lib/assets/Navbar.svelte";
+
+    const errorCode = page.status;
+    const errorMsg = page.error?.message || "An Error Occurred";
+</script>
+
+<div class="main-content center-content flex-col">
+    <Navbar />
+
+    <p class="text-gray-300 text-[10rem] font-bold">{errorCode}</p>
+    <p class="text-gray-300 text-[5rem] font-bold">{errorMsg}</p>
+</div>
