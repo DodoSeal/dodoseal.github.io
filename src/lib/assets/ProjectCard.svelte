e548baa8730d887b5183917716d272136ac6ab7e
DodoSeal <maxhmcd08@gmail.com>
2026-07-17 10:17:41 -0600

    Feat: Begin styling portfolio route - ProjectCard.svelte component is unfinished

---
+++ src/lib/assets/ProjectCard.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    interface ProjectInfo {
+        title: string,
+        description: string,
+        imageSrc: string,
+        tags: string[]
+    };
+
+    const data: ProjectInfo = $props();
+</script>
+
+<div class="max-w-5x h-100 bg-gray-800 rounded-2xl">
+
+</div>
--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import Footer from "$lib/assets/Footer.svelte";
 	import Navbar from "$lib/assets/Navbar.svelte";
+	import ProjectCard from "$lib/assets/ProjectCard.svelte";
 	import { onMount } from "svelte";
     let sitePath: string = $state("");
 
@@ -10,7 +12,10 @@
 
 <div class="main-wrapper">
     <Navbar />
-    <div class="main-content center-content">
-        <p class="text-5xl text-text-light font-bold">{sitePath}</p>
+    <div class="gap-6 grid grid-cols-1 md:grid-cols-2 pt-20">
+        <ProjectCard title="" description="" imageSrc="" tags={[]} />
+        <ProjectCard title="" description="" imageSrc="" tags={[]} />
+        <ProjectCard title="" description="" imageSrc="" tags={[]} />
     </div>
+    <Footer />
 </div>
