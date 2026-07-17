fcf474c47f0948b7be9a402f9cab0ff478c1e9cc
DodoSeal <maxhmcd08@gmail.com>
2026-07-17 10:17:08 -0600

    Fix: Redesign Navbar (again) to be responsive with new `main-wrapper` class

--- src/lib/assets/Navbar.svelte
+++ src/lib/assets/Navbar.svelte
@@ -7,9 +7,9 @@
     });
 </script>
 
-<div class="bg-backdrop top-0 w-screen h-40 md:px-80 py-8 flex-col md:flex-row center-content md:justify-between">
+<div class="bg-backdrop w-full flex-col md:flex-row center-content md:justify-between">
     <!-- Title and Email -->
-    <div class="w-65 h-full center-content md:m-12">
+    <div class="h-full center-content">
         <p class="text-text-light font-xl font-bold m-1 whitespace-nowrap">Max McDaniel</p>
         
         <div class="w-0.5 h-4 opacity-20 shrink-0 bg-gray-300 m-1"></div>
@@ -18,9 +18,9 @@
     </div>
 
     <!-- Navigation Buttons -->
-    <div class="w-75 h-full flex center-content">
-        <a href="/" class="{currentRoute === "/" ? "text-text-light" : "text-text-dark"} font-xl font-bold m-4 hover:text-text-light transition-colors duration-200 cursor-pointer" id="nav-home">Home</a>
-        <a href="/portfolio" class="{currentRoute === "/portfolio" ? "text-text-light" : "text-text-dark"} font-xl font-bold m-4 hover:text-text-light transition-colors duration-200 cursor-pointer" id="nav-portfolio">Portfolio</a>
-        <a href="/contact" class="{currentRoute === "/contact" ? "text-text-light" : "text-text-dark"} font-xl font-bold m-4 hover:text-text-light transition-colors duration-200 cursor-pointer" id="nav-contact">Contact</a>
+    <div class="h-full flex center-content gap-6">
+        <a href="/" class="{currentRoute === "/" ? "text-text-light" : "text-text-dark"} font-xl font-bold  hover:text-text-light transition-colors duration-200 cursor-pointer" id="nav-home">Home</a>
+        <a href="/portfolio" class="{currentRoute === "/portfolio" ? "text-text-light" : "text-text-dark"} font-xl font-bold  hover:text-text-light transition-colors duration-200 cursor-pointer" id="nav-portfolio">Portfolio</a>
+        <a href="/contact" class="{currentRoute === "/contact" ? "text-text-light" : "text-text-dark"} font-xl font-bold  hover:text-text-light transition-colors duration-200 cursor-pointer" id="nav-contact">Contact</a>
     </div>
 </div>
