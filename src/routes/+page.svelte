b73bf4729725fe81f33c2018ad2dc5c26584b35b
DodoSeal <maxhmcd08@gmail.com>
2026-07-17 11:03:24 -0600

    Feat: Site "under construction" notice

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,9 @@
 <div class="main-wrapper">
     <Navbar />
     <PageTitle title="Hey, I am Max! 👋" description={aboutMe}/>
-    <div class="main-content center-content">
+    <div class="main-content center-content text-text-light">
         <!-- Stuff about me goes here btw -->
+        <p class="text-xl md:text-5xl self-start text-yellow-400 text-center">This website is under construction! Feel free to explore my work at <a class="text-blue-500 underline underline-offset-2" target="_blank" href="https://github.com/dodoseal">https://github.com/dodoseal</a></p>
     </div>
     <Footer />
 </div>
