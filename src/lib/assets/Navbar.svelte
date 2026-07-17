1b7591922cf513e2a789ddd2a8b977e6d2744ed8
DodoSeal <maxhmcd08@gmail.com>
2026-07-16 20:26:41 -0600

    Fix: Redesign navbar to be responsive on all screen sizes

--- src/lib/assets/Navbar.svelte
+++ src/lib/assets/Navbar.svelte
@@ -7,12 +7,14 @@
     });
 </script>
 
-<div class="bg-backdrop fixed top-0 w-screen md:w-200 h-40 py-8 flex flex-col md:flex-row items-center place-content-center md:justify-between">
+<div class="bg-backdrop top-0 w-screen h-40 md:px-80 py-8 flex-col md:flex-row center-content md:justify-between">
     <!-- Title and Email -->
-    <div class="w-75 h-full flex center-content">
-        <p class="text-gray-300 font-xl font-bold m-1">Max McDaniel</p>
-        <div class="w-0.5 h-4 bg-gray-300 opacity-20 m-1 rounded-xl"></div>
-        <p id="email-button" class="text-gray-300 font-xl font-bold m-1 hover:underline underline-offset-2 cursor-pointer">me@dodoseal.com</p>
+    <div class="w-65 h-full center-content md:m-12">
+        <p class="text-gray-300 font-xl font-bold m-1 whitespace-nowrap">Max McDaniel</p>
+        
+        <div class="w-0.5 h-4 opacity-20 shrink-0 bg-gray-300 m-1"></div>
+
+        <a href="mailto:me@dodoseal.com" id="email-button" class="text-gray-300 font-xl font-bold m-1 hover:underline underline-offset-2 cursor-pointer">me@dodoseal.com</a>
     </div>
 
     <!-- Navigation Buttons -->
