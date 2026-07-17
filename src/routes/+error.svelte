6587c4c469d5fd973daca6176fdcf9e26da81e89
DodoSeal <maxhmcd08@gmail.com>
2026-07-17 00:05:19 -0600

    Feat: Add red color to error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -9,7 +9,8 @@
 <div class="main-wrapper">
     <Navbar />
     <div class="main-content center-content flex-col">
-        <p class="text-gray-300 text-[10rem] font-bold">{errorCode}</p>
-        <p class="text-gray-300 text-[5rem] font-bold">{errorMsg}</p>
+        <p class="text-red-400 text-[5rem] font-bold">{errorCode}</p>
+        <p class="text-red-400 text-[2.5rem] font-bold">{errorMsg}</p>
+        <a class="p-4 mt-8 rounded-2xl w-48 h-18 center-content cursor-pointer text-text-light text-xl text-center font-bold bg-gray-800 hover:bg-gray-700 transition-all duration-200" href="/">Return Home</a>
     </div>
 </div>
