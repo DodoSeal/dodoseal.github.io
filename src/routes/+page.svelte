ea84b1d9e519dd619ce293ed5c67305b064be521
DodoSeal <maxhmcd08@gmail.com>
2026-07-17 10:59:43 -0600

    Feat: Load `about-me.txt` for landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,12 @@
 	import Footer from "$lib/assets/Footer.svelte";
 	import Navbar from "$lib/assets/Navbar.svelte";
 	import PageTitle from "$lib/assets/PageTitle.svelte";
-	import { onMount } from "svelte";
+	import aboutMe from '$lib/assets/about-me.txt?raw';
 </script>
 
 <div class="main-wrapper">
     <Navbar />
-    <PageTitle title="Hey!, I am Max! 👋" description="Some boring stuff that describes who I am and what I do... yeah let's go with that..."/>
+    <PageTitle title="Hey, I am Max! 👋" description={aboutMe}/>
     <div class="main-content center-content">
         <!-- Stuff about me goes here btw -->
     </div>
